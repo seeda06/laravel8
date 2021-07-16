7684813d0f0fa660e855befff73f36e8de0c76de
seeda06 <86487372+seeda06@users.noreply.github.com>
2021-07-16 12:10:22 +0700

    push to heroku

--- resources/views/test/newgallery.blade.php
+++ resources/views/test/newgallery.blade.php
@@ -2,7 +2,7 @@
 
 <body>
 <h1>gallery of Chvalit 62122420002 ปรียานุช สีดามา </h1>
-ant <a href="newgallery/ant">
+ant <a href="gallery/ant">
 <img src="{{$ant}}" width="250" height="200"></a>
 bird <img src="{{$bird}}" width="250" height="200">
 cat <img src="{{$cat}}" width="250" height="200">
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get( "/gallery" , function(){
-	$ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-    $bird = "https://www.hebergementwebs.com/image/cc/cc8811773d2cdbeb4d46e5550fc455fe.jpg/falcon-and-the-winter-soldier-falcon-minifigure-captain-america.jpg";
+$ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
+$bird = "https://www.hebergementwebs.com/image/cc/cc8811773d2cdbeb4d46e5550fc455fe.jpg/falcon-and-the-winter-soldier-falcon-minifigure-captain-america.jpg";
 $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
 $god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg";
 $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg"; 
@@ -35,4 +35,10 @@ Route::get("/student" , function (){
 Route::get('/table', function () {
     return view('table');
 });
+use App\Http\Controllers\MyProfileController;
 
+Route::get("/myprofile/create",[ MyProfileController::class , "create" ]);
+Route::get("/myprofile/{id}/edit", [ MyProfileController::class , "edit" ] );
+Route::get("/myprofile/{id}", [ MyProfileController::class , "show" ]);
+Route::get( "/newgallery" , [ MyProfileController::class , "gallery" ] );
+Route::get( "/newgallery/ant" , [ MyProfileController::class , "ant" ] );
