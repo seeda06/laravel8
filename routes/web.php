22078c290fbbec6e3fd9e9a260181977c5e91d76
seeda06 <86487372+seeda06@users.noreply.github.com>
2021-07-16 12:22:46 +0700

    push to heroku

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get( "/gallery" , function(){
 $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
